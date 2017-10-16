2ac08f9885b0dbb9ea0f517d4435a6faf4f8aad3
62504 <jkoch.62504@farnborough.ac.uk>
2017-10-16 10:30:31 +0100

    Main stage try 2

--- src/sample/mainScene.java
+++ src/sample/mainScene.java
@@ -22,34 +22,41 @@ public class mainScene extends Application {
         topPane.setStyle("-fx-background-color: navy;");
         topPane.setPadding(new Insets(20,60,0,60));
         HBox.setHgrow(topPane, Priority.ALWAYS);
+        topPane.setAlignment(Pos.CENTER);
+        BorderPane.setAlignment(topPane, Pos.TOP_CENTER);
         root.setTop(topPane);
+
         Button historyButton = new Button("History");
         historyButton.getStyleClass().add("normalButton");
         historyButton.setPrefSize(Integer.MAX_VALUE, 20);
         historyButton.setOnAction((ActionEvent ae) -> error(ae));
         topPane.getChildren().add(historyButton);
+
         Button adjustStockButton= new Button("Adjust Stock");
         adjustStockButton.getStyleClass().add("normalButton");
         adjustStockButton.setPrefSize(Integer.MAX_VALUE, 20);
         adjustStockButton.setOnAction((ActionEvent ae) -> error(ae));
         topPane.getChildren().add(adjustStockButton);
+
         Button newStockButton = new Button("Add New Item");
         newStockButton.getStyleClass().add("normalButton");
         newStockButton.setPrefSize(Integer.MAX_VALUE, 20);
         newStockButton.setOnAction((ActionEvent ae) -> error(ae));
         topPane.getChildren().add(newStockButton);
+
         Button refundButton = new Button("Refund");
         refundButton.getStyleClass().add("normalButton");
         refundButton.setPrefSize(Integer.MAX_VALUE, 20);
         refundButton.setOnAction((ActionEvent ae) -> error(ae));
         topPane.getChildren().add(refundButton);
-        topPane.setAlignment(Pos.CENTER);
-        BorderPane.setAlignment(topPane, Pos.TOP_CENTER);
 
         VBox centerPane = new VBox(20);
         centerPane.setStyle("-fx-background-color: navy;");
         centerPane.setPadding(new Insets(0,60,0,60));
         root.setCenter(centerPane);
+        centerPane.setAlignment(Pos.CENTER);
+        BorderPane.setAlignment(centerPane, Pos.CENTER);
+
         TableView table = new TableView<>();
         table.setPrefSize(400,400);
         table.setItems(purchaseTable);
@@ -74,8 +81,6 @@ public class mainScene extends Application {
         priceColumn.setPrefWidth(90.4);
         table.getColumns().add(priceColumn);
         centerPane.getChildren().add(table);
-        centerPane.setAlignment(Pos.CENTER);
-        BorderPane.setAlignment(centerPane, Pos.CENTER);
 
         HBox bottomPane = new HBox(20);
         bottomPane.setStyle("-fx-background-color: navy;");
@@ -89,9 +94,16 @@ public class mainScene extends Application {
         customerPane.setVgap(10);
         customerPane.setPadding(new Insets(10));
         customerPane.setStyle("-fx-background-color: forestgreen");
+
         Label customerInformation = new Label("Customer Information");
         customerInformation.setPrefWidth(Integer.MAX_VALUE);
         customerPane.add(customerInformation,0,0);
+
+        Button editCustomerInfo = new Button("Edit/New");
+        editCustomerInfo.getStyleClass().add("normalButton");
+        editCustomerInfo.setPrefSize(Integer.MAX_VALUE, 20);
+        editCustomerInfo.setOnAction((ActionEvent ae) -> error(ae));
+        customerPane.add(editCustomerInfo,1,0);
         bottomPane.getChildren().add(customerPane);
 
         Scene scene = new Scene(root, 1024, 768);
